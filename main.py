25a3259103d90eb2b7626b3f65fec930c2637a3d
mikewang0520 <mikewang2000@outlook.com>
2023-12-18 14:23:26 -0500

    Add query_item_by_parameters endpoint to handle query parameters

--- main.py
+++ main.py
@@ -32,3 +32,33 @@ def query_item_by_id(item_id: int) -> Item:
     if item_id not in items:
         raise HTTPException(status_code=404, detail=f"Item with {item_id = } not found")
     return items[item_id]
+
+# Function parameters that are not path parameters can be specified as query parameters in the URL
+# Here we can query items like this /items?count=20
+Selection = dict[
+    str, str | int | float | Category | None
+]  # dictionary containing the user's query arguments
+
+
+@app.get("/items/")
+def query_item_by_parameters(
+    name: str | None = None,
+    price: float | None = None,
+    count: int | None = None,
+    category: Category | None = None,
+) -> dict[str, Selection | list[Item]]:
+    def check_item(item: Item):
+        """Check if the item matches the query arguments from the outer scope."""
+        return all(
+            (
+                name is None or item.name == name,
+                price is None or item.price == price,
+                count is None or item.count != count,
+                category is None or item.category is category,
+            )
+        )
+    selection = [item for item in items.values() if check_item(item)]
+    return {
+        "query": {"name": name, "price": price, "count": count, "category": category},
+        "selection": selection,
+    }
--- test_server.py
+++ test_server.py
@@ -1,3 +1,4 @@
 import requests
 
-print(requests.get("http://127.0.0.1:8000/items/59").json())
+print(requests.get("http://127.0.0.1:8000/items/0").json())
+print(requests.get("http://127.0.0.1:8000/items?name=Nails").json())
